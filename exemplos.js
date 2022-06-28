d2b37973b20d51d0723b3ba719d86a31a0321df7
Yago Renan <byago7408@gmail.com>
2022-06-27 22:00:28 -0500

    versao 0.2

--- exemplos.js
+++ exemplos.js
@@ -9,3 +9,47 @@ function calculaArea2(){
 //saida
  document.getElementById("area").value = area;
 }
+
+function somar(){
+    //recuperação de dados
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat( document.getElementById("numero2").value);
+    var numero3 = parseFloat(document.getElementById("numero3").value);
+    
+    //processamento 
+    var somar = (numero1 + numero2 + numero3);
+   
+
+    //saida 
+
+    document.getElementById("soma").value = somar;
+
+}
+
+function novosalario(){
+    //recuperação de dados
+    var salarioa = parseFloat(document.getElementById("salarioa").value);
+    var porcentagem = parseFloat(document.getElementById("porcentagem").value);
+
+    //processamento
+
+    var nsalario = salarioa + (salarioa * porcentagem)/100;
+
+    //saida
+    document.getElementById("salarionovo").value = nsalario;
+}
+
+function mediaa(){
+    //recuperação de dados
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+    var nota3 = parseFloat(document.getElementById("nota3").value);
+    var nota4 = parseFloat(document.getElementById("nota4").value);
+    console.log("TExt");
+
+    //processamento 
+    var nota= (nota1 + nota2 + nota3 + nota4)/4;
+
+    //saida 
+    document.getElementById("media").value = nota;
+}
